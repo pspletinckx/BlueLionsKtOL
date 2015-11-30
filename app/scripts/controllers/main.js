5076f3b2f1d9187f2893f6c8dc038d50e7b6c2ca
Pieter Pletinckx <pieplay@gmail.com>
2015-11-30 23:20:21 +0100

    Migrating off linux dev box

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -33,6 +33,9 @@ angular.module('statelessScoreboardApp')
 
     $scope.GeneralsFilter="";
 
+    $scope.ScoreGenerals=0;
+    $scope.ScoreLions=0;
+
     //logic
 
     $scope.getTotal = function(stat){ //totals compiler
@@ -70,7 +73,11 @@ angular.module('statelessScoreboardApp')
     $scope.resolvePlayer = function(playerid){
         return $scope.lookuptable;
         //return promise
-    }
+    };
+    $scope.getLions=function(){
+        return $scope.Lions;
+    };
+
     $scope.teamA = [ //representational scoreboard
     {
     	name: 'Bullet0Storm',
@@ -129,8 +136,6 @@ angular.module('statelessScoreboardApp')
             $scope.GeneralsFilter = $scope.GeneralsFilter+member.character_id+",";
             console.log($scope.GeneralsFilter);
 
-
-
                 $scope.GeneralsTR.push(
                 {
                     name: member.name.first,
@@ -154,6 +159,9 @@ angular.module('statelessScoreboardApp')
             if (member.name != null){
             //console.log(member.character_id +" "+member.name.first);
             $scope.lookuptable.set(member.character_id, member.name.first);
+            $scope.GeneralsFilter = $scope.GeneralsFilter+member.character_id+",";
+
+            console.log($scope.GeneralsFilter);
                 $scope.GeneralsVS.push(
                 {
                     name: member.name.first,
@@ -191,38 +199,18 @@ angular.module('statelessScoreboardApp')
     var updateTeamA = function(){};
 
     var timesExecuted = 0;
-
-    var getStatelessData = function(){
-        timesExecuted ++;
-        console.log(timesExecuted);
-    	//http://census.daybreakgames.com/get/ps2:v2/characters_event/?character_id=5428010618020694593&c:limit=10&type=DEATH
-    	$http.jsonp('http://census.daybreakgames.com/s:BlueLegacy/get/ps2:v2/characters_event/?character_id=5428010618035982689&c:limit=10&type=DEATH&callback=JSON_CALLBACK')
-    	.success(function(data){
-            var events = data.characters_event_list;
-            for (var i = data.characters_event_list.length - 1; i >= 0; i--) {
-                $scope.killspam.push(
-                    {
-                        time:events[i].timestamp,
-                        killer:events[i].attacker_character_id,
-                        target:events[i].character_id,
-                        weapon:events[i].attacker_weapon_id
-                    });
-                //console.log(events[i]);
-            };
-        });
-    };
     
     var getScoreData = function(playerids){
         timesExecuted ++;
         console.log(timesExecuted);
-        var timestamp = 1433084852; 
+        var timestamp = 1433116800; //event time
         var killspam =$scope.killspam
         if(killspam.length >0){
             timestamp = $scope.killspam[$scope.killspam.length-1].time; //only get new events
         }
         //var playerids = "5428010618035982689,5428163811558091905,5428147970869687073"; //Bullet0Storm, Gr1mJ4w, RB21
         //http://census.daybreakgames.com/get/ps2:v2/characters_event/?character_id=5428010618020694593&c:limit=10&type=DEATH
-        $http.jsonp('http://census.daybreakgames.com/s:BlueLegacy/get/ps2:v2/characters_event/?character_id='+playerids+'&c:limit=100&type=DEATH&after='+timestamp+'&callback=JSON_CALLBACK')
+        $http.jsonp('http://census.daybreakgames.com/s:BlueLegacy/get/ps2:v2/characters_event/?character_id='+playerids+'&c:limit=1000&type=DEATH,KILL&after='+timestamp+'&callback=JSON_CALLBACK')
         .success(function(data){
             console.log(data);
             var events = data.characters_event_list;
@@ -234,9 +222,55 @@ angular.module('statelessScoreboardApp')
                         target:events[i].character_id,
                         weapon:events[i].attacker_weapon_id
                     });
+                if(($scope.lookuptable.get(events[i].attacker_character_id)!=null)&&($scope.lookuptable.get(events[i].character_id))!=null){
+                    if(events[i].table_type=="kills") $scope.ScoreGenerals +=1;
+                    if(events[i].table_type=="deaths") $scope.ScoreLions ++;
+                };
+                
             };
             console.log(data.returned+" records returned");
         });
+    };
+    var timestamp=0;
+    var UpdateScoreboard = function(){
+        for (var l = $scope.Lions.length - 1; l >= 0; l--) {
+                    $scope.Lions[l].kills =0;
+                    $scope.Lions[l].deaths =0;
+                };
+        for (var l = $scope.GeneralsTR.length - 1; l >= 0; l--) {
+                    $scope.GeneralsTR[l].kills =0;
+                    $scope.GeneralsTR[l].deaths =0;
+                };
+        for (var l = $scope.GeneralsVS.length - 1; l >= 0; l--) {
+                    $scope.GeneralsVS[l].kills =0;
+                    $scope.GeneralsVS[l].deaths =0;
+                };
+
+        for (var i = $scope.killspam.length - 1; i >= 0; i--) {
+                var matchKiller=$scope.killspam[i].killer;
+                var matchTarget=$scope.killspam[i].target;
+            if(($scope.lookuptable.get(matchKiller)!=null)&&($scope.lookuptable.get(matchTarget))!=null){
+                console.log(matchKiller);
+                console.log(matchTarget);
+                for (var l = $scope.Lions.length - 1; l >= 0; l--) {
+                    if($scope.Lions[l].id == matchKiller) $scope.Lions[l].kills ++;
+                    if($scope.Lions[l].id == matchTarget) $scope.Lions[l].deaths ++;
+                };
+
+                for (var l = $scope.GeneralsTR.length - 1; l >= 0; l--) {
+                    if($scope.GeneralsTR[l].id == matchKiller) $scope.GeneralsTR[l].kills ++;
+                    if($scope.GeneralsTR[l].id == matchTarget) $scope.GeneralsTR[l].deaths ++;
+                };
+                for (var l = $scope.GeneralsVS.length - 1; l >= 0; l--) {
+                    if($scope.GeneralsVS[l].id == matchKiller) $scope.GeneralsVS[l].kills ++;
+                    if($scope.GeneralsVS[l].id == matchTarget) $scope.GeneralsVS[l].deaths ++;
+                };
+            };
+
+        };
+    };
+    $scope.playerMatters = function(player){
+        return (player.kills>0||player.deaths>0);
     };
 
     //addOutfitToIndex();
@@ -244,13 +278,14 @@ angular.module('statelessScoreboardApp')
     //getScoreData();
     addBlueLions();
     addGeneralsTR();
-    //addGeneralsVS();
+    addGeneralsVS();
     //addOutfitToIndex();
 
     $interval(function(){
         console.log("Going for new loop");
+        UpdateScoreboard();
         getScoreData($scope.GeneralsFilter);
     }
-    ,10000);
+    ,5000);
 });
 
